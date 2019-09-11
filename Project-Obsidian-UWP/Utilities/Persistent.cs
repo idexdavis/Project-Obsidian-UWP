0a4290d481989a3249b0f62aa8ada6c107014321
TotoroQ <totoroq@outlook.com>
2019-09-11 20:11:54 +0800

    Update

---
+++ Project-Obsidian-UWP/Utilities/Persistent.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Storage;
+
+namespace Project_Obsidian_UWP.Utilities
+{
+    public class Persistent
+    {
+        public static void SaveSettings(string id, string content)
+        {
+            ApplicationDataContainer localSettings = ApplicationData.Current.LocalSettings;
+            localSettings.Values[id] = content;
+        }
+
+        public static Object RetrieveSettings(string id)
+        {
+            ApplicationDataContainer localSettings = ApplicationData.Current.LocalSettings;
+            return localSettings.Values[id];
+        }
+    }
+}
